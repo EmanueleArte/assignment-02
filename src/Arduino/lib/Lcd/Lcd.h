6a223a487ed39400ec911f63d2980c113f09f674
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-11-23 18:41:54 +0100

    Adds and fixes

---
+++ src/Arduino/lib/Lcd/Lcd.h
@@ -0,0 +1,21 @@
+#ifndef __LCD__
+#define __LCD__
+
+#include <LiquidCrystal_I2C.h>
+#include <string.h>
+
+class Lcd {
+  LiquidCrystal_I2C lcd;
+  String lines[4];
+
+public:
+  void init();
+  void print(String text, int line);
+  void clear(int line);
+  void clearAll();
+  void turnOn();
+  void turnOff();
+
+};
+
+#endif
